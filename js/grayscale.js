0d294d9e572f2321a2a05d158537e6829d32a22e
Leonid Dinershtein <leonid@dinershtein.com>
2016-03-29 20:28:09 +0300

    New products

--- js/grayscale.js
+++ js/grayscale.js
@@ -47,7 +47,7 @@ function init() {
         zoom: 15,
 
         // The latitude and longitude to center the map (always required)
-        center: new google.maps.LatLng(40.6700, -73.9400), // New York
+        center: new google.maps.LatLng(67.2752242, 14.5085392),
 
         // Disables the default Google Maps UI components
         disableDefaultUI: true,
@@ -175,7 +175,8 @@ function init() {
 
     // Custom Map Marker Icon - Customize the map-marker.png file to customize your icon
     var image = 'img/map-marker.png';
-    var myLatLng = new google.maps.LatLng(40.6700, -73.9400);
+    var myLatLng = new google.maps.LatLng(67.2752242, 14.5085392);
+
     var beachMarker = new google.maps.Marker({
         position: myLatLng,
         map: map,
